ae51c564c22e2c04c8e49c34780a9a4a4a6243d8
harikrishna2009 <84006431+harikrishna2009@users.noreply.github.com>
2021-06-20 18:27:10 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,58 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+    question.hide();
+    background("yelllow")
+    fill(0)
+    text("Result of the quiz",340,50)
+    text("--------------------------",320,65)
+    Contestant.getPlayerInfo();
+    if (allContestants !== undefined){
+      debugger;
+      var display_Answers = 230
+      fill("blue")
+      textSize(30)
+      text("**Note:Contestant who answer correct is highlighted in green colour")
+      for(var plr in allContestants){
+        var correctAns="2"
+        if(correctAns === allContestants[plr].answer){
+          fill("green")
+        }else
+        fill("red")
+
+        display_Answers+=30
+        textSize(15)
+        text(allContestants[plr].name + ": " + allContestants[plr].answer,250,display_Answers)
+      }
+    }
+  }
+
+}
